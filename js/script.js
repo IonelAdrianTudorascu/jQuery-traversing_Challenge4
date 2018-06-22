587452840c66efbc1d75c35a6482a7ef745a9fa5
Ionel Adrian Tudorascu <adrian_tudorascu@yahoo.ro>
2018-06-22 14:51:41 +0000

    final commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 //waits until page is ready
 $(document).ready(function() {
- 
+    
+    
+ $(".card").click(function(){
+	$(this).toggleClass("highlight");	 
+});
+
+ $("#select_btn").click(function(){
+	$(".card:not(.highlight)").hide();	 
+});
+
+$("#all_btn").click(function(){
+	$(".card").show();	 
+});
 
 }); 
